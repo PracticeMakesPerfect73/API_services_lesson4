c8eebc920b8a39119d16c630b6b43b22ed720615
Alexey <practicemakesperfect73@gmail.com>
2025-03-10 19:58:25 +0900

    Привел к стандарту PEP8

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -8,7 +8,6 @@ def fetch_spacex_launch(launch_id):
     base_url = 'https://api.spacexdata.com/v5/launches/'
     url = f"{base_url}{launch_id}"
     images_path = 'images'
-    extension = get_file_extension(url)
 
     if not os.path.exists(images_path):
         os.mkdir(images_path)
@@ -27,13 +26,17 @@ def fetch_spacex_launch(launch_id):
         image_response = requests.get(image_link)
         image_response.raise_for_status()
 
+        extension = get_file_extension(image_link)
         filename = f"spacex{index}{extension}"
         with open(os.path.join(images_path, filename), 'wb') as file:
             file.write(image_response.content)
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Download spaceX launch images")
-    parser.add_argument("launch_id", nargs = "?", default="latest", help="Идентификатор запуска ")
+    parser = argparse.ArgumentParser(
+        description="Download spaceX launch images")
+    parser.add_argument("launch_id", nargs="?",
+                        default="latest", help="Идентификатор запуска")
     args = parser.parse_args()
 
     user_input = args.launch_id
