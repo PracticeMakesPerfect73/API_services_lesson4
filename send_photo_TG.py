9130d2ed11a906918e014696d5faee495286dfdb
Alexey <practicemakesperfect73@gmail.com>
2025-03-11 21:52:15 +0900

    Вывел данные за функцию
    Вывел создание переменных окружения и инициализацию бота за функцию

--- send_photo_TG.py
+++ send_photo_TG.py
@@ -1,13 +1,18 @@
 import telegram
-from environs import env
+from environs import Env
 
-def send_photo(photo_path):
-    env.read_env()
-    TG_TOKEN = env.str("TG_TOKEN")
-    TG_CHAT_ID = env.str("TG_CHAT_ID")
-    bot = telegram.Bot(token=TG_TOKEN)
-    bot.send_photo(chat_id=TG_CHAT_ID, photo=open(f'{photo_path}', 'rb'))
+env = Env()
+env.read_env()
+
+TG_TOKEN = env.str("TG_TOKEN")
+TG_CHAT_ID = env.str("TG_CHAT_ID")
+
+bot = telegram.Bot(token=TG_TOKEN)
 
 
-if __name__ == "__main__":
-    send_photo(photo_path)
+def send_image(image_path):
+    try:
+        with open(image_path, 'rb') as photo:
+            bot.send_photo(chat_id=TG_CHAT_ID, photo=photo)
+    except Exception as e:
+        print(f"Ошибка при отправке: {e}")
