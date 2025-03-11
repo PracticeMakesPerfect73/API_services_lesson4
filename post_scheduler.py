4d4fd63bf5c611f9a5ea98db23bb3705a98f5227
Alexey <practicemakesperfect73@gmail.com>
2025-03-11 21:52:43 +0900

    Добавил скрипт расписания публикаций

---
+++ post_scheduler.py
@@ -0,0 +1,47 @@
+import random
+import os
+import time
+import argparse
+from environs import Env
+from send_photo_TG import send_image
+
+
+def main():
+    env = Env()
+    env.read_env()
+    images_path = 'images'
+
+    parser = argparse.ArgumentParser(
+        description="Автоматическая публикация фото в Telegram."
+    )
+    parser.add_argument(
+        "--interval",
+        type=int,
+        default=env.int("PUBLICATION_INTERVAL_HOURS", 4),
+        help="Интервал между публикациями в часах "
+             "(по умолчанию из .env или 4 ч)."
+    )
+    args = parser.parse_args()
+
+    publication_interval_hours = args.interval
+
+    while True:
+        images = [os.path.join(images_path, img)
+                  for img in os.listdir(images_path)]
+        random.shuffle(images)
+        secs_in_hour = 3600
+
+        if not images:
+            time.sleep(publication_interval_hours * secs_in_hour)
+            continue
+
+        for image in images:
+            try:
+                send_image(image)
+            except Exception as e:
+                print(f"Ошибка при отправке: {e}")
+            time.sleep(publication_interval_hours * secs_in_hour)
+
+
+if __name__ == "__main__":
+    main()
