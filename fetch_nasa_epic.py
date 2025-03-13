a11bb04ad9ed13958af9c807e13202f081a62ae2
Alexey <practicemakesperfect73@gmail.com>
2025-03-13 22:43:16 +0900

    Переименовал переменные

--- fetch_nasa_epic.py
+++ fetch_nasa_epic.py
@@ -19,14 +19,14 @@ def fetch_nasa_epic(api_key):
 
     response = requests.get(url, params=params)
     response.raise_for_status()
-    images_data = response.json()
-    for index, image_info in enumerate(images_data[:IMAGE_COUNT]):
-        date_obj = datetime.strptime(image_info["date"],
+    epic_images = response.json()
+    for index, epic_entry in enumerate(epic_images[:IMAGE_COUNT]):
+        date_obj = datetime.strptime(epic_entry["date"],
                                      "%Y-%m-%d %H:%M:%S")
         year = "{:04d}".format(date_obj.year)
         month = "{:02d}".format(date_obj.month)
         day = "{:02d}".format(date_obj.day)
-        image_name = image_info["image"]
+        image_name = epic_entry["image"]
         image_url_path = (
             f"EPIC/archive/natural/{year}/{month}/{day}/png/{image_name}.png"
         )
